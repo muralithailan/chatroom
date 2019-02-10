662ee80ad31d4a8dcf68fd0b0b0d451017dc95b6
CORP\z022839 <balamurali.thailan@rntbci.com>
2019-02-10 13:28:51 +0530

    main application update

--- src/main/java/com/websocker/chat/ChatWebsocketDemoApplication.java
+++ src/main/java/com/websocker/chat/ChatWebsocketDemoApplication.java
@@ -2,7 +2,11 @@ package com.websocker.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Chat room is a simple chat application
+ * @author z022839
+ *
+ */
 @SpringBootApplication
 public class ChatWebsocketDemoApplication {
 
